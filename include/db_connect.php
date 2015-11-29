c36bd8985d7bbb2e7f39ce14c411c47ac94defb8
Mirzairl <mirza1995@hotmail.com>
2015-11-29 19:44:04 +0000

    Update db_connect.php

--- include/db_connect.php
+++ include/db_connect.php
@@ -1,14 +1,8 @@
 <?php
-function db_connect($tradesmendirectory)
+function db_connect($email_signup)
 {
-   $host_name = "localhost";
-   $user_name = "root";
-   $password = "usbw";
-   $db_name = "tradesmendirectory";
-   $db_link = mysql_connect($host_name, $user_name, $password)
-      or die("Could not connect to $host_name");
-   mysql_select_db($tradesmendirectory)
-      or die("Could not select database $db_name");
+   $db_link = mysql_connect('localhost', 'root', '') or die("I couldn't connect to your database, please make sure your info is correct!");
+   mysql_select_db('email_signup') or die("I couldn't find the database table ($table) make sure it's spelt right!");
    return $db_link;
 }
 ?>
